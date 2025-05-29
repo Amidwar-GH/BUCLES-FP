c54457408440017b3e17c27391859c25344f78ae
Amidwar <amidwar05@gmail.com>
2025-05-28 23:17:56 -0500

    Cambio de variable para legibilidad y mejora visual

--- EjercioBucles1.cpp
+++ EjercioBucles1.cpp
@@ -2,20 +2,22 @@
 using namespace std;
 
 int main(){
-	int n, N, mayor=0; 
-	cout<<Digite la cantidad de numeros: ; cin>>n; 
+	int Cn, N, mayor=0; 
+	cout<<"BIENVENIDO"<<endl;
+	cout<<"Digite la cantidad de numeros: "; cin>>Cn; 
 	
-	for(int i=1; i<=n; i++){
-		cout<<Digite el numero <<i<< : ; cin>>N;
+	for(int i=1; i<=Cn; i++){
+		cout<<"Digite el numero "<<i<< ":" ; cin>>N;
 		
 		while(N<=0){
-			cout<<Digite otro numero mayor a cero: ; cin>>N;
+			cout<<"Digite otro numero mayor a cero: "; cin>>N;
 		}
 		
 		if(N > mayor){
 			mayor = N;
 		}
 	}
-	cout<<El numero mayor es: <<mayor<<endl;
+	cout<<"El numero mayor es: "<<mayor<<endl;
+	cout<<"GRACIAS, programa terminado."<<endl;
 	return 0;
 }
