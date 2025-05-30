57ebcf2f271314934fab6de6c4a33517573e0db3
Amidwar <amidwar05@gmail.com>
2025-05-29 23:11:06 -0500

    Agregando validacion para numeros negativos, Agregando continuidad de programa con bucle, una NUEVA CONDICION para usuario final

--- BucleSumaNaturales.cpp
+++ BucleSumaNaturales.cpp
@@ -2,13 +2,26 @@
 using namespace std;
 
 int main(){
-	int n, S=0; // NUMEROS // SUMA 
-	cout<<Digite el numero que desea saber la suma: ; cin>>n;
+	int n, S=0, ACUMULADOR=0; // NUMEROS // SUMA 
+	char Respuesta,Respuesta1;
+	do{
+		cout<<"Digite el termino que desea saber la suma: "; cin>>n;
+	if(n<0){
+		n=n*-1;
+	}
 	
 	for(int i=0; i<=n; i++){
 		S=S+i;
-	}cout<<La suma es <<S<<endl;
+	}cout<<"La suma de los "<<n<<" primeros terminos es: "<<S<<endl;
+	ACUMULADOR=ACUMULADOR+S;
+	cout<<"Desea saber otra suma? (S/N) "; cin>>Respuesta;
+	}while(Respuesta != 'N');
+	cout<<"Deseas saber la suma de tus numeros (S/N)? " ; cin>>Respuesta1;
 	
-	
+	if(Respuesta = 'S'){
+		cout<<"La suma de tus numeros es "<<ACUMULADOR;
+	}else{
+		cout<<"GRACIAS, PROGRAMA TERMINADO.";
+	}
 	return 0;
 }
