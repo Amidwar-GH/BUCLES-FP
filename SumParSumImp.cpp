470ff335d58419ccaeedd73716be74d825e8a422
Amidwar <amidwar05@gmail.com>
2025-05-30 23:13:40 -0500

    Agregando opcion a una interfaz de eleccion a suma de numeros pares, impares o ambos

--- SumParSumImp.cpp
+++ SumParSumImp.cpp
@@ -2,15 +2,40 @@
 using namespace std;
 
 int main(){
-	int n, S=0; // NUMERO LIMITE // SUMA// 
-	cout<<Digite un numero para saber la suma de sus impares: ; cin>>n;
+	int n, SumaDePares=0, SumaDeImpares=0, opcion; // NUMERO LIMITE // SUMA// 
+	cout<<"Digite un numero para saber la suma de sus impares: "<<endl;
+	cin>>n;
 	
 	for(int i=1; i<=n; i++){
 		if(i % 2 != 0){
-			S=S+i;
+			SumaDeImpares+=i;
+		}else{
+			SumaDePares+=i;
 		}
 	}
-	cout<<La suma de los impares es <<S<<endl;
+	cout<<"\nQue suma desea saber?: "<<endl;
+	cout<<"1. suma de pares"<<endl;
+	cout<<"2. suma de impares"<<endl;
+	cout<<"3. ambas sumas"<<endl;
+	cin>>opcion;
+	switch(opcion){
+		case 1:
+			cout<<"La suma de numeros pares es "<<SumaDePares;
+			break;
+		
+		case 2:
+			cout<<"La suma de impares es: "<<SumaDeImpares;
+			break;
+		
+		case 3:
+			cout<<"La suma de pares es "<<SumaDePares<<" y la suma de impares es "<<SumaDeImpares;
+			break;
+			
+		default:
+			cout<<"Opcion no valida."<<endl;
+			break;		
+	}
+	cout<<"\nEl PROGRAMA FINALIZO,GRACIAS";
 	
 	return 0; 
 }
