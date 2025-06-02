59b8e8ff4d85595612080bcf754c919e1e120981
Amidwar <amidwar05@gmail.com>
2025-06-01 20:19:57 -0500

    Modificando nueva funcionalidad de par o impar

--- EjerFact.cpp
+++ EjerFact.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 int main(){
 	int F,N,i;
-	
-	cout<<Digite el numero que desea saber su factorial: ; cin>>N;
+	char Respuesta;
+	cout<<"Digite el numero que desea saber su factorial: "; cin>>N;
 	F=1;
 	if(N<0){
 		N=-1*N;
@@ -13,8 +13,17 @@ int main(){
 	for(i=1; i<=N; i++){
 		F=F*i;
 	}
-	cout<<El factorial de << N << es: << F <<endl;
-	
+	cout<<"El factorial de "<< N <<" es: "<< F <<endl;
+	cout<<"\nDeseas saber si el factorial es par o impar (S/N)?  "; cin>>Respuesta;
+	if(Respuesta == 'S' || Respuesta == 's'){
+		if(F % 2 == 0){
+			cout<<F<<" es par ";
+		}else{
+			cout<<F<<" es impar ";
+		}
+	}else{
+		cout<<"Gracias, PROGRAMA TERMINADO.";
+	}
 	
 return 0;
 }
