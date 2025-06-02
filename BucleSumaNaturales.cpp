75f3474a1ff50880e3dd1a980a4718eb7e405145
Amidwar <amidwar05@gmail.com>
2025-06-02 10:18:19 -0500

    Agregando variables mas claras, y mejora de igualdad de caracter

--- BucleSumaNaturales.cpp
+++ BucleSumaNaturales.cpp
@@ -2,23 +2,23 @@
 using namespace std;
 
 int main(){
-	int n, S=0, ACUMULADOR=0; // NUMEROS // SUMA 
+	int termino, S=0, ACUMULADOR=0; // NUMEROS // SUMA 
 	char Respuesta,Respuesta1;
 	do{
-		cout<<"Digite el termino que desea saber la suma: "; cin>>n;
-	if(n<0){
-		n=n*-1;
+		cout<<"Digite el termino que desea saber la suma: "; cin>>termino;
+	if(termino<0){
+		termino=termino*-1;
 	}
 	
-	for(int i=0; i<=n; i++){
+	for(int i=0; i<=termino; i++){
 		S=S+i;
-	}cout<<"La suma de los "<<n<<" primeros terminos es: "<<S<<endl;
+	}cout<<"La suma de los "<<termino<<" primeros terminos es: "<<S<<endl;
 	ACUMULADOR=ACUMULADOR+S;
 	cout<<"Desea saber otra suma? (S/N) "; cin>>Respuesta;
 	}while(Respuesta != 'N');
 	cout<<"Deseas saber la suma de tus numeros (S/N)? " ; cin>>Respuesta1;
 	
-	if(Respuesta = 'S'){
+	if(Respuesta1 == 'S'){
 		cout<<"La suma de tus numeros es "<<ACUMULADOR;
 	}else{
 		cout<<"GRACIAS, PROGRAMA TERMINADO.";
